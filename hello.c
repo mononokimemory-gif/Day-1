e74c16c7a18ccfb29ce5e5bc19563942121b32ce
mononokimemory-gif <mononokimemory@gmail.com>
2025-10-05 15:51:29 +0200

    day 1: hello.c

---
+++ hello.c
@@ -0,0 +1,1 @@
+#include<stdio.h>int main( ){char name[50];int age;printf("Enter your name: ");scanf("%s", &name);printf("Enter your age: ");scanf("%d", &age);printf("Hello %s you are %d year old", name, age);return 0;}
